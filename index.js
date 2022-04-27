f35d711d9e25739fc8ab0df9b74cf8c232086b16
Jorge García González <jorgegarcia.java@gmail.com>
2022-04-27 15:48:54 +0200

    Final

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { dbConnection } = require('./database/config');
 require ('dotenv').config();
 const cors = require('cors');
 
-
 //Crear el servidor Express
 const app = express();
 
@@ -27,6 +26,11 @@ app.use('/api/hospitales', require('./routes/hospitales'));
 app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + 'public/index.html'));
+});
 
 
 app.listen(process.env.PORT, () => {
